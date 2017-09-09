a1a97bf63815f681c43bdf39b9bf1e0efdecf7d9
Jobin Lawrance <jobinlawrance@gmail.com>
2017-09-10 04:57:07 +0530

    refactor and add test case to HomPresenterImplTest

--- app/src/test/java/com/jobinlawrance/pics/home/HomePresenterImplTest.kt
+++ app/src/test/java/com/jobinlawrance/pics/home/HomePresenterImplTest.kt
@@ -1,23 +1,24 @@
 package com.jobinlawrance.pics.home
 
-import com.jobinlawrance.pics.application.AppComponent
+import com.jobinlawrance.pics.application.DaggerAppComponent
 import com.jobinlawrance.pics.application.NetModule
 import com.jobinlawrance.pics.home.dagger.DaggerHomeComponent
 import com.jobinlawrance.pics.retrofit.mock.MockPhotoResponses
 import io.reactivex.android.plugins.RxAndroidPlugins
 import io.reactivex.schedulers.Schedulers
-import it.cosenonjaviste.daggermock.DaggerMockRule
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.*
+import java.net.ConnectException
 
 /**
  * Created by jobinlawrance on 8/9/17.
  */
 class HomePresenterImplTest {
+    lateinit var mockWebServer: MockWebServer
+    lateinit var baseUrl: String
 
     companion object {
-        val mockWebServer = MockWebServer()
 
         @JvmStatic
         @BeforeClass
@@ -25,40 +26,40 @@ class HomePresenterImplTest {
             // Tell RxAndroid to not use android main ui thread scheduler
             RxAndroidPlugins.setInitMainThreadSchedulerHandler { Schedulers.trampoline() }
 
-            println("Before class")
-            mockWebServer.start()
         }
 
         @JvmStatic
         @AfterClass
         fun tearDown() {
             RxAndroidPlugins.reset()
-            mockWebServer.shutdown()
         }
     }
 
-    lateinit var appComponent: AppComponent
+//    lateinit var appComponent: AppComponent
 
-    @get:Rule
-    val daggerMock = DaggerMockRule<AppComponent>(AppComponent::class.java, NetModule(mockWebServer.url("").toString(), HashMap())).set {
-        println("Inside dagger mock")
-        appComponent = it
-    }
+//    @get:Rule
+//    val daggerMock = DaggerMockRule<AppComponent>(AppComponent::class.java, NetModule(baseUrl, HashMap())).set {
+//        println("Inside dagger mock")
+//        appComponent = it
+//    }
 
     @Before
     fun beforeEachTest() {
+        mockWebServer = MockWebServer()
+        mockWebServer.start()
 
+        baseUrl = mockWebServer.url("").toString()
     }
 
     @After
     fun afterEachTest() {
-
+        mockWebServer.shutdown()
     }
 
-    @Test
-    fun testDaggerMock() {
-        println("Url is ${appComponent.getRetrofit().baseUrl()}")
-    }
+//    @Test
+//    fun testDaggerMock() {
+//        println("Url is ${appComponent.getRetrofit().baseUrl()}")
+//    }
 
     @Test
     fun testLoadingPage() {
@@ -66,7 +67,7 @@ class HomePresenterImplTest {
 
         val presenter =
                 DaggerHomeComponent.builder()
-                        .appComponent(appComponent)
+                        .appComponent(DaggerAppComponent.builder().netModule(NetModule(baseUrl.toString(), HashMap())).build())
                         .build()
                         .providePresenter()
 
@@ -87,6 +88,49 @@ class HomePresenterImplTest {
 
         // Check if as expected
         robot.assertViewStateRendered(loadingFirstPageState, firstPage)
+    }
+
+    @Test
+    fun testLoadingFirstFailsWithConnectionError() {
+        //
+        // Prepare mock server to deliver mock response on incoming http request
+        //
+        mockWebServer.shutdown()
+
+        val presenter =
+                DaggerHomeComponent.builder()
+                        .appComponent(DaggerAppComponent.builder().netModule(NetModule(baseUrl, HashMap())).build())
+                        .build()
+                        .providePresenter()
+
+        val robot = HomeViewRobot(presenter)
+
+        //
+        // We are ready, so let's start: fire an intent
+        //
+        robot.fireLoadFirstPageIntent()
+
+        //
+        // we expect that 2 view.render() events happened with the following HomeViewState:
+        // 1. show loading indicator
+        // 2. show error indicator
+        //
+        val loadingFirstPageState = HomeViewState.Builder().firstPageLoading(true).build()
+        val firstPageError =
+                HomeViewState.Builder()
+//                        .firstPageError(retrofit2.adapter.rxjava2.HttpException(
+//                                Response.error<String>(ResponseBody.create(null, ""),
+//                                        okhttp3.Response.Builder()
+//                                                .code(404)
+//                                                .protocol(Protocol.HTTP_1_1)
+//                                                .request(Request.Builder().url("http://localhost/").build())
+//                                                .message("Client Error")
+//                                                .build())))
+                        .firstPageError(ConnectException())
+                        .build()
+
+        // Check if as expected
+        robot.assertViewStateRendered(loadingFirstPageState, firstPageError)
     }
 
 }
