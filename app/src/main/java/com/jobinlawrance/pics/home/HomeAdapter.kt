a1ede83462c0848dce138c4ee1744bc1f803e8ea
Jobin Lawrance <jobinlawrance@gmail.com>
2017-09-12 23:27:28 +0530

    create HomeViewAdapter and render data in HomeFragment

---
+++ app/src/main/java/com/jobinlawrance/pics/home/HomeAdapter.kt
@@ -0,0 +1,49 @@
+package com.jobinlawrance.pics.home
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.jobinlawrance.pics.R
+import com.jobinlawrance.pics.extras.Utils.Companion.inflate
+import com.jobinlawrance.pics.retrofit.data.PhotoResponse
+import kotlinx.android.synthetic.main.home_viewholder.view.*
+
+/**
+ * Created by jobinlawrance on 12/9/17.
+ */
+class HomeAdapter : RecyclerView.Adapter<HomeAdapter.HomeViewHolder>() {
+
+    private var items: List<PhotoResponse>? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeViewHolder {
+        return HomeViewHolder(parent.inflate(R.layout.home_viewholder))
+    }
+
+    override fun getItemCount(): Int = items?.size ?: 0
+
+    override fun onBindViewHolder(holder: HomeViewHolder?, position: Int) {
+        holder?.bind(items!![position])
+    }
+
+    fun setItems(items: List<PhotoResponse>) {
+        val oldItems = this.items
+        this.items = items
+
+        if (oldItems == null) {
+            notifyDataSetChanged()
+        } else {
+            //TODO: use DiffUtils for pagination
+        }
+    }
+
+    class HomeViewHolder(item: View) : RecyclerView.ViewHolder(item) {
+        fun bind(photoResponse: PhotoResponse) = with(itemView) {
+            Glide.with(itemView.context)
+                    .load(photoResponse.urls?.regular)
+                    .apply(RequestOptions.centerCropTransform())
+                    .into(imageView)
+        }
+    }
+}
--- app/src/main/java/com/jobinlawrance/pics/home/HomeFragment.kt
+++ app/src/main/java/com/jobinlawrance/pics/home/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.jobinlawrance.pics.home
 
 import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +11,7 @@ import com.jobinlawrance.pics.application.MyApplication
 import com.jobinlawrance.pics.extras.Utils.Companion.inflate
 import com.jobinlawrance.pics.home.dagger.DaggerHomeComponent
 import io.reactivex.Observable
+import kotlinx.android.synthetic.main.fragment_home.*
 import timber.log.Timber
 
 /**
@@ -17,8 +19,20 @@ import timber.log.Timber
  */
 class HomeFragment : MviFragment<HomeContract.View, HomeContract.Presenter>(), HomeContract.View {
 
+    lateinit var homeAdapter: HomeAdapter
+    lateinit var gridLayoutManager: GridLayoutManager
+
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        homeAdapter = HomeAdapter()
+        gridLayoutManager = GridLayoutManager(activity, 1)
+
         return container?.inflate(R.layout.fragment_home)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        recyclerView.adapter = homeAdapter
+        recyclerView.layoutManager = gridLayoutManager
     }
 
     override fun createPresenter(): HomeContract.Presenter {
@@ -32,5 +46,26 @@ class HomeFragment : MviFragment<HomeContract.View, HomeContract.Presenter>(), H
 
     override fun render(viewState: HomeViewState) {
         Timber.d("render : %s", viewState)
+
+        if (viewState.loadingFirstPage) {
+            renderProgress()
+        } else if (viewState.firstPageError != null) {
+            renderFirstPageError(viewState.firstPageError)
+        } else {
+            renderData(viewState)
+        }
+    }
+
+    private fun renderProgress() {
+        progressBar.visibility = View.VISIBLE
+    }
+
+    private fun renderFirstPageError(throwable: Throwable) {
+
+    }
+
+    private fun renderData(viewState: HomeViewState) {
+        progressBar.visibility = View.GONE
+        homeAdapter.setItems(viewState.data)
     }
 }
