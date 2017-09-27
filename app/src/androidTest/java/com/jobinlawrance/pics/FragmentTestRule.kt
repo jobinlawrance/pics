c89f7e153c95af3ea6f83143f204917585dcc46c
Jobin Lawrance <jobinlawrance@gmail.com>
2017-09-27 17:45:46 +0530

    refactor FragmentTestRule

--- app/src/androidTest/java/com/jobinlawrance/pics/FragmentTestRule.kt
+++ app/src/androidTest/java/com/jobinlawrance/pics/FragmentTestRule.kt
@@ -1,39 +1,45 @@
 package com.jobinlawrance.pics
 
+import android.content.Intent
 import android.support.test.rule.ActivityTestRule
 import android.support.v4.app.Fragment
 import com.jobinlawrance.TestActivity
-import junit.framework.Assert
-import java.lang.IllegalArgumentException
 
 /**
  * Created by jobinlawrance on 25/9/17.
  */
 class FragmentTestRule<F : Fragment>(val fragmentClass: Class<F>) : ActivityTestRule<TestActivity>(TestActivity::class.java, true, false) {
 
-    lateinit var fragment: F
 
-    override fun afterActivityLaunched() {
-        super.afterActivityLaunched()
+//    override fun afterActivityLaunched() {
+//        activity.runOnUiThread({
+//            try {
+////                activity.fragment = fragment
+//                activity.supportFragmentManager.beginTransaction()
+//                        .add(R.id.fragment_container, fragment)
+//                        .commit()
+//
+//            } catch (e: Exception) {
+//                when (e) {
+//                    is IllegalAccessException,
+//                    is InstantiationException,
+//                    is IllegalArgumentException -> {
+//                        Assert.fail("${this.javaClass.simpleName}: Could not insert ${fragmentClass.simpleName} into TestActivity: ${e.message}")
+//                    }
+//                    else -> throw e
+//                }
+//            }
+//        })
+//    }
 
-        activity.runOnUiThread({
-            try {
-                //Instantiate and insert the fragment into the container layout
-                val manager = activity.supportFragmentManager
-                fragment = fragmentClass.newInstance()
-                activity.fragment = fragment
+    override fun launchActivity(startIntent: Intent?): TestActivity {
 
-            } catch (e: Exception) {
-                when (e) {
-                    is IllegalAccessException,
-                    is InstantiationException,
-                    is IllegalArgumentException -> {
-                        Assert.fail("${this.javaClass.simpleName}: Could not insert ${fragmentClass.simpleName} into TestActivity: ${e.message}")
-                    }
-                    else -> throw e
-                }
-            }
-        })
+        val intent = if (startIntent == null) Intent() else startIntent
+        intent.putExtra("FragmentClass", fragmentClass)
+
+        return super.launchActivity(intent)
     }
+
+    fun getFragment(): F = activity.fragment as F
 
 }
--- app/src/debug/java/com/jobinlawrance/TestActivity.kt
+++ app/src/debug/java/com/jobinlawrance/TestActivity.kt
@@ -1,29 +1,36 @@
 package com.jobinlawrance
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.annotation.VisibleForTesting
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
-import com.jobinlawrance.pics.R
-import com.jobinlawrance.pics.home.HomeFragment
+import android.widget.FrameLayout
 
 /**
  * Created by jobinlawrance on 25/9/17.
  * This is a test activity defined in debug manifest which is used for inflating a fragment for testing
  */
-@VisibleForTesting
+@VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
 class TestActivity : AppCompatActivity() {
 
     lateinit var fragment: Fragment
 
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        val frameLayout = FrameLayout(this)
-//        frameLayout.id = R.id.fragment_container
-        setContentView(R.layout.activity_main)
+        val frameLayout = FrameLayout(this)
+        frameLayout.id = 6542 //some random id
+        setContentView(frameLayout)
+
+        val fragmentClass = intent.getSerializableExtra("FragmentClass") as Class<Fragment>
+
+        fragment = fragmentClass.newInstance()
 
         supportFragmentManager.beginTransaction()
-                .add(R.id.fragment_container, HomeFragment())
+                .add(frameLayout.id, fragment)
                 .commit()
     }
+
+
 }
