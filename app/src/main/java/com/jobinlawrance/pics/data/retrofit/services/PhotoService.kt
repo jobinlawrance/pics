e7e0586388e43d8909738ac25191328eb035ff5d
Jobin Lawrance <jobinlawrance@gmail.com>
2017-10-09 19:09:02 +0530

    add method to download image in Photo service along with custom download-interceptor header

--- app/src/main/java/com/jobinlawrance/pics/data/retrofit/services/PhotoService.kt
+++ app/src/main/java/com/jobinlawrance/pics/data/retrofit/services/PhotoService.kt
@@ -1,8 +1,13 @@
 package com.jobinlawrance.pics.data.retrofit.services
 
+import com.jobinlawrance.downloadprogressinterceptor.DOWNLOAD_IDENTIFIER_HEADER
 import com.jobinlawrance.pics.data.retrofit.model.PhotoResponse
 import io.reactivex.Observable
+import okhttp3.ResponseBody
 import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Streaming
+import retrofit2.http.Url
 
 /**
  * Created by jobinlawrance on 5/9/17.
@@ -10,4 +15,9 @@ import retrofit2.http.GET
 interface PhotoService {
     @GET("photos")
     fun getPhotos(): Observable<List<PhotoResponse>>
+
+    @GET
+    @Streaming
+    fun downloadPhoto(@Url url: String, @Header(DOWNLOAD_IDENTIFIER_HEADER) identifier: String): Observable<ResponseBody>
+
 }
